52055aa0a06b67a55e8a67eb3058a931a125132c
ben <yu.shuming0829@gmail.com>
2022-07-14 22:25:57 +0800

    backend order

---
+++ shuming-yu/src/components/OrderModal.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true" ref="modal">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-primary text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>訂單細節</span>
+          </h5>
+          <button type="button" class="btn-close"
+                  data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md-4">
+              <h3>用戶資料</h3>
+              <table class="table">
+                <tbody v-if="gotOrder.user">
+                  <tr>
+                    <th style="width: 100px;">姓名</th>
+                    <td>{{ gotOrder.user.name }}</td>
+                  </tr>
+                  <tr>
+                    <th>Email</th>
+                    <td>{{ gotOrder.user.email }}</td>
+                  </tr>
+                  <tr>
+                    <th>電話</th>
+                    <td>{{ gotOrder.user.tel }}</td>
+                  </tr>
+                  <tr>
+                    <th>地址</th>
+                    <td>{{ gotOrder.user.address }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="col-md-8">
+              <h3>訂單細節</h3>
+              <table class="table">
+                <tbody>
+                  <tr>
+                    <th style="width: 100px">訂單編號</th>
+                    <td>{{ gotOrder.id }}</td>
+                  </tr>
+                  <tr>
+                    <th>下單時間</th>
+                    <td>{{ $filters.date(gotOrder.create_at) }}</td>
+                  </tr>
+                  <tr>
+                    <th>付款時間</th>
+                    <td>
+                      <span v-if="gotOrder.paid_date">
+                        {{ $filters.date(gotOrder.paid_date) }}
+                      </span>
+                      <span v-else>時間不正確</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>付款狀態</th>
+                    <td>
+                      <strong v-if="gotOrder.is_paid == true" class="text-success">已付款</strong>
+                      <strong v-else class="text-danger">尚未付款</strong>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>總金額</th>
+                    <td>
+                      {{ $filters.currency(gotOrder.total) }}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <h3>選購商品</h3>
+              <table class="table">
+                <thead>
+                  <tr>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in gotOrder.products" :key="item.id">
+                    <th>
+                      {{ item.product.title }}
+                    </th>
+                    <td>
+                      {{ item.qty }} / {{ item.product.unit }}
+                    </td>
+                    <td class="text-end">
+                      {{ item.total}}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary"
+                  data-bs-dismiss="modal">取消
+          </button>
+          <button type="button" class="btn btn-primary"
+                  @click="$emit('confirm-order', gotOrder)">確認</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from "bootstrap/js/dist/modal";
+
+export default {
+  data() {
+    return {
+      modal: {},
+      gotOrder: {},
+    };
+  },
+
+  props: {
+    propOrder: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+
+  watch: {
+    propOrder() {
+      this.gotOrder = this.propOrder;
+      console.log(this.gotOrder);
+    }
+  },
+
+  methods: {
+    showModal() {
+      this.modal.show();
+    },
+    hideModal() {
+      this.modal.hide();
+    },
+  },
+
+  mounted() {
+    this.modal = new Modal(this.$refs.modal);
+  },
+};
+</script>
--- shuming-yu/src/views/Orders.vue
+++ shuming-yu/src/views/Orders.vue
@@ -1,4 +1,5 @@
 <template>
+
   <table class="table mt-4">
     <thead>
       <tr>
@@ -11,43 +12,87 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Time</td>
-        <td>email</td>
-        <td class="text-right">200</td>
-        <td class="text-right">100</td>
-        <td>
-          <span class="text-success">啟用</span>
-        </td>
-        <td>
-          <div class="btn-group">
-            <button class="btn btn-outline-primary btn-sm">編輯</button>
-            <button class="btn btn-outline-danger btn-sm">刪除</button>
-          </div>
-        </td>
-      </tr>
+      <template  v-for="item in orders" :key="item.id">
+        <tr>
+          <td>{{ $filters.date(item.create_at) }}</td>
+          <td>{{ item.user.email }}</td>
+          <td class="text-right">
+            <ul class="list-unstyled">
+              <li v-for="product in item.products" :key="product.id">
+                {{ product.product.title }} - 數量 : {{ product.qty }} {{ product.product.unit }}
+              </li>
+            </ul>
+          </td>
+          <td class="text-right">{{ $filters.currency(item.total) }}</td>
+          <td>
+            <span class="text-success" v-if="item.is_paid == true">已付款</span>
+            <span class="text-danger" v-else>未付款</span>
+          </td>
+          <td>
+            <div class="btn-group">
+              <button class="btn btn-outline-primary btn-sm" @click="openOrderModal(false, item)">檢視</button>
+              <button class="btn btn-outline-danger btn-sm">刪除</button>
+            </div>
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
+  <OrderModal ref="orderModal"
+              :propOrder="tempOrder"
+              @confirm-order="confirmOrder"></OrderModal>
 </template>
 
 <script>
+import OrderModal from "@/components/OrderModal.vue";
+
 export default {
+
   data() {
     return {
       orders: [], // 訂單資訊
       pagination: {}, // 分頁
+      tempOrder: {},  // 外層資料傳送接收
+      isNew: false,
     };
   },
+
+  components: {
+    OrderModal,
+  },
+
   methods: {
+    
     getOrders() {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders`;
       this.$http.get(api).then((res) => {
-        //console.log(res.data);
+        // console.log(res.data);
         this.orders = res.data.orders;
         this.pagination = res.data.pagination;
+        console.log(this.orders);
       });
     },
+    
+    openOrderModal(isNew, item) {
+      this.tempOrder = { ...item };
+      this.isNew = isNew;
+      // console.log(this.isNew, this.tempOrder);
+
+      const openModal = this.$refs.orderModal;
+      openModal.showModal();
+    },
+
+    confirmOrder(item) {
+      this.tempOrder = item;
+
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders/${ item.id }`;
+      this.$http.put(api, { data: this.tempOrder })
+          .then((res) =>{
+            console.log(res);
+          })
+    },
   },
+
   created() {
     this.getOrders();
   },
